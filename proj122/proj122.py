05e550691cd1cb4e26de76a9347ff023e51b5dda
Sowndarya2295 <69381952+Sowndarya2295@users.noreply.github.com>
2021-12-30 17:06:46 +0530

    Add files via upload

---
+++ proj122/proj122.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+X = np.load('image.npz')['arr_0']
+y = pd.read_csv("labels.csv")["labels"]
+print(pd.Series(y).value_counts())
+classes = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+nclasses = len(classes)
